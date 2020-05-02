deb46c50087633f6f781c74270d71a07a037dde3
Hubsoft <macielrsf@gmail.com>
2020-05-02 11:45:07 -0300

    enviroment variables implemented

--- api/src/config/mail.js
+++ api/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-    host: 'smtp.mailtrap.io',
-    port: 2525,
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
     secure: false,
     auth: {
-        user: '7cd8a45b402391',
-        pass: 'daa476c5d68861',
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
     },
     default: {
         from: 'Equipe GoBarber <noreply@gobarber.com>'
--- api/src/config/sentry.js
+++ api/src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-    dsn: 'https://66a132b71fb9491196cdadd288b16c60@o385624.ingest.sentry.io/5218722' 
+    dsn: process.env.SENTRY_DSN 
 };
